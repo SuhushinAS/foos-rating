aab237e3134d15253cb8f8fa12bab8d81e35d770
Alexander Suhushin <suhushinas@hotmail.com>
2022-09-10 13:19:24 +0700

    layout

--- src/modules/common/scroll/index.js
+++ src/modules/common/scroll/index.js
@@ -1,4 +1,5 @@
 import baron from 'baron';
+import {Base} from 'modules/common/base';
 import {component} from 'utils/component';
 import './style.less';
 
@@ -6,25 +7,43 @@ import './style.less';
  * Class Scroll.
  * Класс для примера.
  */
-class Scroll {
+class Scroll extends Base {
   /**
    * Конструктор класса для примера.
    * @param {*} root Элемент.
    * @return {void}
    */
   constructor(root) {
-    const {baronVId, direction = 'v'} = root.dataset;
+    super(root);
 
-    if (!baronVId) {
+    this.scrollList = [
       baron({
-        bar: '.scroll__bar',
-        direction,
+        bar: '.scroll__bar_v',
+        barOnCls: 'scroll_scrollbar_v',
+        direction: 'v',
         impact: 'scroller',
         root,
         scroller: '.scroll__scroller',
-        track: '.scroll__track',
-      });
-    }
+        track: '.scroll__track_v',
+      }),
+      baron({
+        bar: '.scroll__bar_h',
+        barOnCls: 'scroll_scrollbar_h',
+        direction: 'h',
+        impact: 'scroller',
+        root,
+        scroller: '.scroll__scroller',
+        track: '.scroll__track_h',
+      }),
+    ];
+  }
+
+  destroy() {
+    super.destroy();
+
+    this.scrollList.forEach((baron) => {
+      baron.dispose();
+    });
   }
 }
 
